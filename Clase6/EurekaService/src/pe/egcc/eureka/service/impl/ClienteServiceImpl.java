9c5d658513d12505164052a61ebb3cc18ec5df60
Gustavo Coronel <gcoronelc@gmail.com>
2016-11-24 22:31:56 -0500

    JDBC

--- Clase6/EurekaService/src/pe/egcc/eureka/service/impl/ClienteServiceImpl.java
+++ Clase6/EurekaService/src/pe/egcc/eureka/service/impl/ClienteServiceImpl.java
@@ -1,6 +1,8 @@
 package pe.egcc.eureka.service.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import pe.egcc.eureka.db.AccesoDB;
 import pe.egcc.eureka.model.Cliente;
 import pe.egcc.eureka.service.spec.ClienteServiceSpec;
@@ -19,7 +21,30 @@ public class ClienteServiceImpl implements ClienteServiceSpec{
     Connection cn = null;
     try {
       cn = AccesoDB.getConnection();
-      
+      String sql = "select chr_cliecodigo, vch_cliepaterno,"
+              + "vch_cliematerno, vch_clienombre,"
+              + "chr_cliedni, vch_clieciudad, "
+              + "vch_cliedireccion, vch_clietelefono,"
+              + "vch_clieemail from cliente "
+              + "where chr_cliecodigo = ?";
+      PreparedStatement pstm = cn.prepareStatement(sql);
+      pstm.setString(1, codigo);
+      ResultSet rs = pstm.executeQuery();
+      // Se espera una sola fila
+      if(rs.next()){
+        bean = new Cliente();
+        bean.setCodigo(codigo);
+        bean.setPaterno(rs.getString("vch_cliepaterno"));
+        bean.setMaterno(rs.getString("vch_cliematerno"));
+        bean.setNombre(rs.getString("vch_clienombre"));
+        bean.setDni(rs.getString("chr_cliedni"));
+        bean.setCiudad(rs.getString("vch_clieciudad"));
+        bean.setDireccion(rs.getString("vch_cliedireccion"));
+        bean.setTelefono(rs.getString("vch_clietelefono"));
+        bean.setEmail(rs.getString("vch_clieemail"));
+      }
+      rs.close();
+      pstm.close();
     } catch (Exception e) {
       throw new RuntimeException(e.getMessage());
     } finally{
